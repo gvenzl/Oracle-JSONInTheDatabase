ce57e20642796b289ac65323423bf2a654c38436
Gerald Venzl <gerald.venzl@gmail.com>
2015-07-13 12:49:40 +0100

    Use executemany for tweets load
    Use executemany for tweets load rather than execute.
    Better exception handling

--- examples/twitterstreaming/python/stream.py
+++ examples/twitterstreaming/python/stream.py
@@ -1,22 +1,24 @@
+"""
+@author: gvenzl
+"""
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 import cx_Oracle
 
-db_host = "<host name>"
-db_port = "1521"
-db_name = "<DB name>"
-db_batchSize = 100
+db_host = "<host name>"               # Database host name/IP address
+db_port = "1521"                      # Database listener port (default 1521)
+db_name = "<DB name/service>"         # Database name or Database Service name
+db_user = "<database user>"           # Database user name
+db_password = "<database password>"   # Database user password
+db_table = "TWITTER"                  # Table name for streaming tweets (will be created if it doesn't exist)
+db_col   = "tweet"                    # Column name containing the tweets
+db_batchSize = 100                    # Load <n> many tweets at once (for better load performance)
 
-db_user = "<database user>"
-db_password = "<database password>"
-db_table = "TWITTER"       # Define your table name where you would like to store your tweets
-db_col   = "tweet"         # Define the table name column where you would like to store your tweets
-
-twitter_consumer_token = "<Consumer token>"
+twitter_consumer_token =  "<Consumer token>"
 twitter_consumer_secret = "<Consumer secret>"
-twitter_access_token = "<Access token>"
-twitter_access_secret = "<Access secret>"
+twitter_access_token =    "<Access token>"
+twitter_access_secret =   "<Access secret>"
 
 class DBLoader:
     """Loads data into the database"""
@@ -38,24 +40,27 @@ class DBLoader:
         result = cur.fetchall()
         # Table count returns 0, we need to create the table first
         if (result[0][0] == 0):
-            cur.execute("CREATE TABLE " + db_table + " (" + db_col + " CLOB CONSTRAINT " + db_table + "_valid_json CHECK (" + db_col + " IS JSON))")
-            print("New table created: " + db_table)
+            try:
+                cur.execute("CREATE TABLE " + db_table + " (" + db_col + " CLOB CONSTRAINT " + db_table + "_valid_json CHECK (" + db_col + " IS JSON))")
+            except cx_Oracle.Error as err:
+                print("Error creating table " + db_table + ": " + err.args[0].message)
+                raise err
+            else:
+                print("New table created: " + db_table)
         
     def load(self, data):
-        self._tweets.append(data)
+        self._tweets.append((data,))
         
         # If inserts reach commit size issue a commit
         if (len(self._tweets) == db_batchSize):
             self._persist()
 
     def _persist(self):
-        # Make this use executemany instead of execute
-        for tweet in self._tweets:
-            var = self._cursor.var(cx_Oracle.CLOB)
-            var.setvalue(0,tweet)
-            self._cursor.execute(None, tweet=var)
+        self._cursor.setinputsizes(cx_Oracle.CLOB)
+        self._cursor.executemany(None, self._tweets)
         self._conn.commit()
         self._tweets.clear()
+        print(str(db_batchSize) + " tweets loaded.")
     
 class TweetsListener(StreamListener):
     """TweetsListener listens to incoming tweets and loads them into the DB.
@@ -79,4 +84,7 @@ auth.set_access_token(twitter_access_token, twitter_access_secret)
 
 # Connect to the Twitter stream
 stream = Stream(auth, TweetsListener(), Timeout=None)
-stream.sample()
+try:
+    stream.sample()
+except (KeyboardInterrupt, SystemExit):
+    print("Good bye!")
