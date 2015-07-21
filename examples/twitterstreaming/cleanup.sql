151f4477fb13327493d510eb083af12d34ad8027
Gerald Venzl <gerald.venzl@gmail.com>
2015-07-21 15:36:44 +0100

    Update cleanup.sql

--- examples/twitterstreaming/cleanup.sql
+++ examples/twitterstreaming/cleanup.sql
@@ -1,3 +1,4 @@
+-- Close pluggable database. You may not need that if you are running in a non-CDB environment.
 ALTER PLUGGABLE DATABASE TEST CLOSE;
 
 -- Drop pluggable database. You may not need that if you are running in a non-CDB environment.
