98d4dffd9927d746b51ecb92ba900c6ecbc20ab6
Gerald Venzl <gerald.venzl@gmail.com>
2015-08-11 17:51:46 -0700

    Create cleanup.sql

---
+++ examples/indexingjson/nodejs/cleanup.sql
@@ -0,0 +1,9 @@
+-- Close pluggable database. You may not need that if you are running in a non-CDB environment.
+ALTER PLUGGABLE DATABASE TEST CLOSE;
+
+-- Drop pluggable database. You may not need that if you are running in a non-CDB environment.
+DROP PLUGGABLE DATABASE TEST INCLUDING DATAFILES;
+
+-- Drop user STREAMER. You may not need that if you are running in a pluggable database environment.
+DROP USER STREAMER CASCADE;
+DROP USER BIUSER;
